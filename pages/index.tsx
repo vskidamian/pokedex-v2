ffdb329f8a0c917f73c6bfd0475529719d8c61b0
vskidamian <jakubowski1337@gmail.com>
2022-09-24 17:05:38 +0200

    feat: request returning list with images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,31 @@
-import type { GetStaticProps, NextPage } from 'next'
-import { get } from '../services/requests';
+import type { GetStaticProps, NextPage } from "next";
+import { get } from "../services/requests";
 
-const PokemonList: NextPage = ({pokemon}) => {
-  console.log(pokemon)
-  return (
-    <div>
-      {JSON.stringify(pokemon)}
-    </div>
-  )
-}
+const PokemonList: NextPage = ({ pokemon }) => {
+  console.log(pokemon);
+  return <div>{JSON.stringify(pokemon)}</div>;
+};
 
 export async function getStaticProps() {
-  const res = await get({url: 'pokemon'});
+  try {
+    const pokemonList = await get({ uri: "pokemon" });
+    const pokemonListData = await Promise.all(pokemonList.results.map(async (pokemon) => await get({ url: pokemon.url })));
 
-  return {
-    props: {
-      pokemon: res,
-    },
-  };
+    return {
+      props: {
+        pokemon: {
+          ...pokemonList,
+          results: pokemonListData.map((pokemon) => ({
+            id: pokemon.id,
+            name: pokemon.species.name,
+            img: pokemon.sprites.other["official-artwork"].front_default,
+          })),
+        },
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-
-export default PokemonList
+export default PokemonList;
--- services/requests.ts
+++ services/requests.ts
@@ -1,15 +1,18 @@
 import axios from "axios"
 
 type Get =  {
-    url: string;
+    url?: string;
+    uri?: string;
     params?: any;
 }
 
-export const get = async ({url, params = {}}: Get) => {
+export const get = async (reqParams: Get) => {
+    const url = reqParams.url ? reqParams.url : `https://pokeapi.co/api/v2/${reqParams.uri}`;
+
     try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/${url}`, params);
-        return await res.data
+        const res = await axios.get(url, reqParams.params);
+        return res.data
     } catch (error) {
-        console.error('xd:', error);
+        console.error('Error:', error);
     }
 }
